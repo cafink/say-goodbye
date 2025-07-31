bbf4c57d3aa86a64283fb41069319e9e38feb12a
Carl Fink <cafink@gmail.com>
2025-07-31 15:01:25 -0500

    Show hearts

--- classes/side.py
+++ classes/side.py
@@ -15,7 +15,12 @@ class Side:
 
     def load_details_from_xml(self, xml):
         self.strawberry_count = xml.attrs["TotalStrawberries"]
+        self.got_heart = xml.attrs["HeartGem"] == 'true'
 
     def print_details(self):
-        print("  " + self.letter + "-side")
+        print("  " + self.letter + "-side", end="")
+        if self.got_heart:
+            print(" ♥")
+        else:
+            print()
         print("    Strawberry count: " + str(self.strawberry_count))
