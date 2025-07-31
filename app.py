0fa04ac5bd90445aae3d2fcd0744a8a707591d5b
Carl Fink <cafink@gmail.com>
2025-07-31 15:52:47 -0500

    Create basic Flask app

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from flask import Flask, render_template
 from classes.save import Save
 
 save = Save("saves/0.celeste (175 save)")
-save.print_details()
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", save=save)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
